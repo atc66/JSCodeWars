0f0a89308cfb14800ab1c8dbdbd837cca7e41fed
aciukurescu <andrewciukurescu@gmail.com>
2018-04-30 12:22:04 -0400

    finding next donation needed to get average donation

--- script.js
+++ script.js
@@ -483,22 +483,47 @@
 // }
 
 
-arr = [-8,0,2,5];
+// arr = [-8,0,2,5];
 
 
-function indexEqualsValueSearch(arr) {
-	var sol;
-	for (i=0; i< arr.length; i++){
-  		if(arr[i]==i){
-  			sol = i;
-  			break;
-  		} else {
-  			sol = -1;
-  		}
-	}
-	return sol;
+// function indexEqualsValueSearch(arr) {
+// 	var sol;
+// 	for (i=0; i< arr.length; i++){
+//   		if(arr[i]==i){
+//   			sol = i;
+//   			break;
+//   		} else {
+//   			sol = -1;
+//   		}
+// 	}
+// 	return sol;
+// }
+
+// The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. After doing the math he found 149. He thinks that he made a mistake. Could you help him?
+// if dons = [14, 30, 5, 7, 9, 11, 15] then new_avg(dons, 30) --> 149
+
+// The function new_avg(arr, navg) should return the expected donation (rounded up to the next integer) that will permit to reach the average navg.
+// Should the last donation be a non positive number (<= 0) John wants us to throw an error
+// (return Nothing in Haskell, return None in F# and Ocaml, return -1 in C, Fortran, Nim, echo ERRORin Shell)
+// so that he clearly sees that his expectations are not great enough.
+// Notes:
+// all donations are numbers (integers or floats depending on the language), arr can be empty.
+// See examples below to see which error is to be thrown.
+
+arr = [14, 30, 5, 7, 9, 11, 15]
+newavg = 30
+
+function newAvg(arr, newavg) {
+  sum = 0;
+  donors = arr.length +1;
+    for(i=0; i < arr.length; i++){
+      sum += arr[i];
+    }
+    var needed = ((donors)*(newavg));
+    sol = ((needed)-(sum));
+    if (sol <= 0){
+      return ERROR
+    } else {
+    return Math.ceil(sol);
+  }
 }
-
-
-
-
