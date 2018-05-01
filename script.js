b77fe87bc0619847e2b05b04a7609aeb84808136
aciukurescu <andrewciukurescu@gmail.com>
2018-04-30 21:24:37 -0400

    doris coding challenge

--- script.js
+++ script.js
@@ -510,20 +510,75 @@
 // all donations are numbers (integers or floats depending on the language), arr can be empty.
 // See examples below to see which error is to be thrown.
 
-arr = [14, 30, 5, 7, 9, 11, 15]
-newavg = 30
+// arr = [14, 30, 5, 7, 9, 11, 15]
+// newavg = 30
 
-function newAvg(arr, newavg) {
-  sum = 0;
-  donors = arr.length +1;
-    for(i=0; i < arr.length; i++){
-      sum += arr[i];
-    }
-    var needed = ((donors)*(newavg));
-    sol = ((needed)-(sum));
-    if (sol <= 0){
-      return ERROR
-    } else {
-    return Math.ceil(sol);
-  }
+// function newAvg(arr, newavg) {
+//   sum = 0;
+//   donors = arr.length +1;
+//     for(i=0; i < arr.length; i++){
+//       sum += arr[i];
+//     }
+//     var needed = ((donors)*(newavg));
+//     sol = ((needed)-(sum));
+//     if (sol <= 0){
+//       return ERROR
+//     } else {
+//     return Math.ceil(sol);
+//   }
+// }
+
+
+// Given a string that includes alphanumeric characters ('3a4B2d') return the expansion of that string: The numeric values represent the occurance of each letter preceding that numeric value. There should be no numeric characters in the final string. Empty strings should return an empty string.
+
+// The first occurance of a numeric value should be the number of times each character behind it is repeated, until the next numeric value appears.
+
+// stringExpansion('3D2a5d2f') === 'DDDaadddddff'
+// stringExpansion('3abc') === 'aaabbbccc'      // correct
+// stringExpansion('3abc') !== 'aaabc'          // wrong
+// stringExpansion('3abc') !== 'abcabcabc'      // wrong
+// If there are two consecutive numeric characters the first one is ignored.
+
+// stringExpansion('3d332f2a') === 'dddffaa'
+// If there are two consecutive alphabetic characters then the first character has no effect on the one after it.
+
+// stringExpansion('abcde') === 'abcde'
+// Your code should be able to work for both lower and capital case letters.
+
+// stringExpansion('') === ''
+
+// var s = '3az4B2d'
+// var s = 'a2bcde'
+
+// function stringExpansion(s) {
+// 	// set a blank string
+// 	var result="";
+// 	for(i=0; i< s.length; i++){
+// 		// if s[i] is
+// 		// console.log(!isNaN(s[i]))
+// 		if (!isNaN(s[i]) == true && !isNaN(s[i+1]) == false){
+// 			x = s[i+1].repeat(s[i])
+// 			// console.log(x)
+// 			result += x
+// 		} else if (!isNaN(s[i]) == false && !isNaN(s[i+1]) == true){
+// 			z = s[i]
+// 			result += z
+// 		}
+// 	}
+// 	return result
+// }
+
+function doris(i){
+	// for(i=0;i<=100; i++){
+		if (i%2 !== 0){
+			console.log(i +'weird')
+		} else if (i%2 === 0 && i>=2 && i<=5){
+			console.log(i +'Not weird')
+		} else if (i%2 === 0 && i>=6 && i<=20){
+			console.log(i +'Weird')
+		}else if (i%2 === 0 && i>20){
+			console.log(i + 'Not Weird')
+		}
+	// }
 }
+
