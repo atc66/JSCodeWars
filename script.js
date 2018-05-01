3af26c60a3a2d61d88c58bba6ad375339a5928e9
aciukurescu <andrewciukurescu@gmail.com>
2018-05-01 17:23:47 -0400

    population growth

--- script.js
+++ script.js
@@ -657,6 +657,39 @@
 //   	}
 // }
 
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+// More generally given parameters:
+// p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+// the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+// aug is an integer, percent a positive or null number, p0 and p are positive integers (> 0)
+// Examples:
+// nb_year(1500, 5, 100, 5000) -> 15
+// nb_year(1500000, 2.5, 10000, 2000000) -> 10
+// Note: Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
+p0 = 1500
+percent = 5
+aug = 100
+p = 5000
+
+// function nbYear(p0, percent, aug, p) {
+//     var currPop = p0;
+//     Year = 0;
+//     while(currPop < p){ 
+//     	Year++; 
+//     	currPop = currPop + (currPop * (percent/100)) + aug; 
+//     }
+//     return Year
+// }
+
+function nbYear(p0, percent, aug, p) {
+	for(i=0; p0<p;i++){
+		p0 = p0+(p0*(percent/100))+aug;
+	}
+	return i
+}
+
+
 // week = [[100,75,49],[20,15,20],[10,15,10],[50,50,20],[20,15,10],[20,15,10],[20,15,10],1]
 
 // console.log(week[week.length-1])
